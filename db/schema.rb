443278779466f7595196a292aa305c522d5d3b26
Davis Ma <davishma@gmail.com>
2018-11-02 16:19:51 -0400

    WIP board game rounds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_02_145818) do
+ActiveRecord::Schema.define(version: 2018_11_02_195431) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -91,6 +91,12 @@ ActiveRecord::Schema.define(version: 2018_11_02_145818) do
     t.index ["event_id"], name: "index_reviews_on_event_id"
     t.index ["group_id"], name: "index_reviews_on_group_id"
     t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
+  create_table "rounds", force: :cascade do |t|
+    t.datetime "date", null: false
+    t.bigint "boardgame_id", null: false
+    t.index ["boardgame_id"], name: "index_rounds_on_boardgame_id"
   end
 
   create_table "users", force: :cascade do |t|
