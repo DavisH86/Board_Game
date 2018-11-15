24bb99d87ab4806d464c9c57a9a2fb9aacb6b268
Davis Ma <davishma@gmail.com>
2018-11-15 17:42:51 -0500

    fix search Event

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -6,7 +6,11 @@ class SearchesController < ApplicationController
     if @type == "Boardgame"
       @results = Boardgame.where('title ILIKE?', "%#{params[:keyword]}%")
     elsif @type == "Event_Location"
-      @results = Location.where('name ILIKE ? OR address ILIKE ?', "%#{params[:keyword]}%", "%#{params[:keyword]}%").first.events
+      if @results = Location.where('name ILIKE ? OR address ILIKE ?', "%#{params[:keyword]}%", "%#{params[:keyword]}%").first != nil
+        @results = Location.where('name ILIKE ? OR address ILIKE ?', "%#{params[:keyword]}%", "%#{params[:keyword]}%").first.events
+      else
+        @results = []
+      end
     elsif @type == "Group"
       @results = Group.where('name ILIKE?', "%#{params[:keyword]}%")
     end
